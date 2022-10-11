8cb24d79cfcae5755d50a52f16aa4bf66555f12a
variance1 <variance45@gmail.com>
2022-10-11 21:55:31 +0000

    WIP: fastapi app.py

---
+++ app.py
@@ -0,0 +1,70 @@
+from typing import Union, Optional
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from omegaconf import OmegaConf
+import random
+
+from stable_diffusion_webui.modules.processing import StableDiffusionProcessing
+from stable_diffusion_webui.modules.processing import StableDiffusionProcessingTxt2Img
+from stable_diffusion_webui.modules.processing import StableDiffusionProcessingImg2Img
+
+class Generation(BaseModel):
+    model: Optional[str] = "sd-v1-4"
+    prompt: Optional[str] = "An astronaut riding a horse."
+    iterations: Optional[int] = 1
+    steps: Optional[int] = 50
+    seed: Optional[int] = random.randint(1, 99999)
+    cfg_scale: Optional[float] = 7.5
+    seamless: Optional[bool] = False
+    init_img: Optional[str] = None
+    init_mask: Optional[str] = None
+    negative_prompt: Optional[str] = None
+    restore_faces: Optional[bool] = False
+    tiling: Optional[bool] = False
+    strength: Optional[float] = 0.75
+
+
+app = FastAPI()
+
+"""
+@app.get("/")
+def run_root():
+    return root()
+@app.post("/")
+def root():
+    return "hello world"
+"""
+@app.post("/api")
+async def root(generation: Generation = None):
+
+    if not generation:
+        generation = StableDiffusionProcessing
+
+    arg_dict = {
+        "sd_model": generation.model + ".ckpt",
+        "prompt": generation.prompt,
+        "iterations": generation.iterations,
+        "steps": generation.steps,
+        "seed": generation.seed,
+        "cfg_scale": generation.cfg_scale,
+        "seamless": generation.seamless,
+        "init_img": generation.init_img, # For img2img
+        "init_mask": generation.init_mask, # For inpainting
+        "strength": generation.strength
+    }
+
+    #generation = Generate(weights=weights, config=config)
+    #output = generation.prompt2png(**arg_dict, outdir="outputs/web_out")
+
+    #return send_file(output[0][0], mimetype="image/png")
+    #return "something"
+    return FileResponse(output[0][0])
+
+#async def Generate(weights: str, config: str):
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="216.153.51.112", port=8001, log_level="debug")
--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ AUTOMATIC1111_REPO = 'https://github.com/AUTOMATIC1111/stable-diffusion-webui.gi
 AUTOMATIC1111_LOCAL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'stable_diffusion_webui')
 AUTOMATIC1111_COMMIT_ID = '6c36fe5719a824fa18f6ad3e02727783f095bc5f'
 
-repo = git.Repo.clone_from(AUTOMATIC1111_REPO, AUTOMATIC1111_LOCAL_PATH, no_checkout=True)
-repo.git.checkout(AUTOMATIC1111_COMMIT_ID)
+if not os.path.exists(AUTOMATIC1111_LOCAL_PATH):
+    repo = git.Repo.clone_from(AUTOMATIC1111_REPO, AUTOMATIC1111_LOCAL_PATH, no_checkout=True)
+    repo.git.checkout(AUTOMATIC1111_COMMIT_ID)
 
 # Import AUTOMATIC1111 install script, now that it's cloned
 # Note: environment is misspelled "enviroment" in AUTOMATIC1111's install script
@@ -29,3 +30,7 @@ prepare_enviroment()
 # Get models from S3
 for model in S3_MODEL_CHECKPOINTS:
     wget.download(S3_URL + model, out=os.path.join(AUTOMATIC1111_LOCAL_PATH, 'models/Stable-diffusion'))
+
+# Run webui for testing
+os.chdir(AUTOMATIC1111_LOCAL_PATH)
+#os.system('python3 launch.py --share')
